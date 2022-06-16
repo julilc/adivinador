0b0b74f82fead2e94f0e6583155115235e772a69
julilc <julilopezceratto1@gmail.com>
2022-06-16 13:53:02 -0300

    solucion adivinanzas

--- js/eleccion.js
+++ js/eleccion.js
@@ -3,48 +3,77 @@ let adivinanzaHTML = document.getElementById("adivinanza")
 let respuesta1 = document.getElementById("respuesta1");
 let respuesta2 = document.getElementById("respuesta2");
 let respuesta3 = document.getElementById("respuesta3");
-let respuestaHTML = document.querySelectorAll(".botones");
 let btnAcertijo = document.getElementById("continuarAceritjo")
 let alertaResp = document.getElementById("alertaRespuesta");
-let btnAcertijo2 = document.getElementById("continuarAceritjo2");
 let nombreUsuariols = sessionStorage.getItem('Nombre Usuario');
-function adivinanzaP() {
-    adivinanzaHTML.innerText = listAdivinanzas[0].acertijo;
-    respuesta1.innerText = ("Manzana");
-    respuesta3.innerText = ("Limon");
-    respuesta2.innerText = (listAdivinanzas[0].respuesta);
+let indiceAdiv = 0;
+let contResp = 3;
+
+function adivinanzaP(indiceAdiv, countResp) {
+    if (indiceAdiv == 3)() => {
+        adivinanzaHTML.innerText = ("Felicidades " + nombreUsuariols + "!\nRespuesta correcta! Se han terminado las adivinanzas por hoy! Prueba las otras funciones del sitio!");
+        adivinanzaHTML.style.color = "green";
+        alertaResp.style.display = "none";
+        btnAcertijo.innerText = "volver a inicio"
+        btnAcertijo.style.alignSelf = "center";
+    }
+    countResp = 3
+    adivinanzaHTML.innerText = (listAdivinanzas[indiceAdiv].acertijo)
+    respuesta1.innerText = (listAdivinanzas[indiceAdiv].respInco1);
+    respuesta3.innerText = (listAdivinanzas[indiceAdiv].respInco2);
+    respuesta2.innerText = (listAdivinanzas[indiceAdiv].respuesta);
     adivinanzaHTML.style.color = "aliceblue"
-    alertaResp.style.display="none";
-    btnAcertijo.style.display="none";
-    btnAcertijo2.style.display="none";
-    setTimeout(()=>{
-        alert("psita: empieza con P")
-    }, 4000);
+    alertaResp.style.display = "none";
+    btnAcertijo.style.display = "none";
+    validarRespuesta(indiceAdiv, countResp);
 };
 
-
-respuestaHTML.forEach(boton => {
-    boton.addEventListener("click", (e) => {
-        let respuesta = e.target.innerText;
-        console.log(respuesta);
-        for (let i = 0; i < 2; i++, respuesta != listAdivinanzas[0].respuesta) {
-            if (respuesta == listAdivinanzas[0].respuesta) {
+function validarRespuesta(indiceAdiv, countResp) {
+    let btnAcertijo = document.getElementById("continuarAceritjo")
+    let respuestaHTML = document.querySelectorAll(".botones");
+    respuestaHTML.forEach(boton => {
+        boton.addEventListener("click", (e) => {
+            let respuesta = e.target.innerText;
+            if (respuesta == listAdivinanzas[indiceAdiv].respuesta) {
                 adivinanzaHTML.innerText = ("Respuesta correcta! pasemos a la siguiente adivinanza!");
                 adivinanzaHTML.style.color = "green";
-                alertaResp.style.display="none";
+                alertaResp.style.display = "none";
                 btnAcertijo.style.display = "block";
-                btnAcertijo.style.alignSelf = "center"
-
-                break;
+                btnAcertijo.style.alignSelf = "center";
+            } else {
+                alertaResp.innerText = ("respuesta incorrecta, le quedan " + (countResp--) + " intentos");
+                alertaResp.style.color = "red";
+                alertaResp.style.display = "block";
+                if (countResp < 0) {
+                    alert("se te acabaron las posibilidades")
+                    location.href = "menu.html"
+                }
             }
-            alertaResp.innerText = ("respuesta incorrecta, le quedan " + (2 - i) + " intentos");
-            alertaResp.style.color = "red";
-            alertaResp.style.display="block";
-        };
-    })
-});
+        })
+    });
+}
+adivinanzaP(indiceAdiv)
 
-/*acertijo2*/
+btnAcertijo.addEventListener("click", () => {
+    console.log(indiceAdiv);
+    if (indiceAdiv == 2) {
+        adivinanzaHTML.innerText = ("Felicidades " + nombreUsuariols + "!\nRespuesta correcta! Se han terminado las adivinanzas por hoy! Prueba las otras funciones del sitio!");
+        adivinanzaHTML.style.color = "green";
+        alertaResp.style.display = "none";
+        btnAcertijo.style.display = "none";
+        let respuestas=document.getElementById("opResp");
+        respuestas.style.display="none";
+        
+    } 
+    else {
+        indiceAdiv++;
+        adivinanzaP(indiceAdiv);
+    }
+
+})
+
+/*
+/*acertijo2
 btnAcertijo.addEventListener("click", () => {
     adivinanzaHTML.innerText = listAdivinanzas[1].acertijo;
     respuesta1.innerText = ("Rosa");
@@ -79,7 +108,7 @@ respuestaHTML.forEach(boton => {
         };
     })
 });
-/*acertijo2*/
+/*acertijo3
 btnAcertijo2.addEventListener("click", () => {
     adivinanzaHTML.innerText = listAdivinanzas[2].acertijo;
     respuesta1.innerText = ("Barril");
@@ -90,13 +119,14 @@ btnAcertijo2.addEventListener("click", () => {
     btnAcertijo.style.display="none";
     btnAcertijo2.style.display="none";
     setTimeout(()=>{
-        alert("psita: cuando se taladra una pared se forma un...")
+        alert("pista: cuando se taladra una pared se forma un...")
     }, 4000);
 });
 respuestaHTML.forEach(boton => {
     boton.addEventListener("click", (e) => {
         let respuesta = e.target.innerText;
         console.log(respuesta);
+        console.log(listAdivinanzas[2].respuesta);
         for (let i = 0; i < 2; i++, respuesta != listAdivinanzas[2].respuesta) {
             if (respuesta == listAdivinanzas[2].respuesta) {
                 adivinanzaHTML.innerText = ("Felicidades " + nombreUsuariols + "!\nRespuesta correcta! Se han terminado las adivinanzas por hoy! Prueba las otras funciones del sitio!");
@@ -113,4 +143,4 @@ respuestaHTML.forEach(boton => {
             alertaResp.style.visibility="visible";
         };
     })
-});
+})*/
--- js/listAdivinanzas.js
+++ js/listAdivinanzas.js
@@ -1,12 +1,15 @@
-function adivinanza(acertijo, respuesta){
+function adivinanza(acertijo, respuesta, respInco1, respInco2, pista){
     this.acertijo=acertijo;
     this.respuesta=respuesta;
+    this.respInco1=respInco1;
+    this.respInco2=respInco2;
+    this.pista=pista;
     respuesta.toUpperCase();
 }
 const adivinanzas=[]
-adivinanzas.push(new adivinanza("Blanca por dentro, verde por fuera.\nSi no sabes, espera.\n¿Qué fruta es?","Pera"));
-adivinanzas.push(new adivinanza("Bonita flor que gira buscando el sol. ¿Qué es?","Girasol"));
-adivinanzas.push(new adivinanza("cuanto más le quitas más grande es", "Agujero"));
+adivinanzas.push(new adivinanza("Blanca por dentro, verde por fuera.\nSi no sabes, espera.\n¿Qué fruta es?","Pera","Limón","Naranja","empieza con P"));
+adivinanzas.push(new adivinanza("Bonita flor que gira buscando el sol. ¿Qué es?","Girasol","Margarita","Rosa","se forma sumando Gira+Sol"));
+adivinanzas.push(new adivinanza("cuanto más le quitas más grande es", "Agujero","Anillo","Barril","cuando se taladra una pared se forma un..."));
 const guardarLocal=(clave,valor)=>{localStorage.setItem(clave,valor)};
 guardarLocal("Adivinanzas",JSON.stringify(adivinanzas));
 
